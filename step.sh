542c57970e2022602c969d08c5b920da1924e56c
Andrew Marmion <andrew.marmion@gmail.com>
2019-10-09 19:40:23 +0100

    Create step.yml and step.sh

--- step.sh
+++ step.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+if [ ! -e $browserstack_username ]; then
+  echo "Please provide your Browserstack Username"
+  exit 1
+fi
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+if [ ! -e $browserstack_access_key ]; then
+  echo "Please provide the location of your GoogleService-Info.plist"
+  exit 1
+fi
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+if [ ! -e $apk_ipa_filepath ]; then
+  echo "Please provide the path for the IPA or APK that you wish to upload."
+  echo "For IPA it is usually \$BITRISE_IPA_PATH"
+  echo "For APK it is usually \$BITRISE_APK_PATH"
+  exit 1
+fi
+
+curl -u "$browserstack_username:$browserstack_access_key" -X POST https://api-cloud.browserstack.com/app-live/upload -F "file=@$apk_ipa_filepath"
